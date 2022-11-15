a4554af5a100362624499269c7890f48d5881aa8
Thilina Sandakelum <thilinas.19@cse.mrt.ac.lk>
2022-11-15 12:43:55 +0530

    jwt modifies added

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,49 +4,54 @@ const AdminUpdatingDetailsModel = require("../models/AdminUpdatingDetailsModel")
 const UserStatusModel = require("../models/UserStatusModel");
 
 const handleAddAdmin = async (req, res) => {
-  // console.log("hello");
-  // console.log("handle add admin calling, ", req.body);
+  const { usertype } = req.data;
   const { name, walletaddress, email, mobilenumber, DOB } = req.body;
   // console.log("passing data", name, walletaddress, email, mobilenumber, DOB);
-
-  try {
-    const user = await UserModel.findOne({
-      walletaddress: walletaddress,
-    });
-
-    if (user) {
-      return res.status(200).json({
-        message: "Already exists with this Wallet Address!",
-        AdminName: user.name,
-
-        view: user,
-      });
-    } else {
-      const newUser = await UserModel.create({
+  if (usertype === "Super Admin") {
+    try {
+      const user = await UserModel.findOne({
         walletaddress: walletaddress,
-        usertype: "Admin",
-        name: name,
-        username: "Admin",
-        propic: null,
       });
 
-      const newAdmin = await AdminDetailsModel.create({
-        userid: newUser._id,
-        email: email,
-        DOB: DOB,
-        mobilenumber: mobilenumber,
-      });
+      if (user) {
+        return res.status(200).json({
+          message: "Already exists with this Wallet Address!",
+          AdminName: user.name,
 
-      return res.status(201).json({
-        message: "Successfully Added!",
-        name: newUser.name,
-        email: newAdmin.email,
+          view: user,
+        });
+      } else {
+        const newUser = await UserModel.create({
+          walletaddress: walletaddress,
+          usertype: "Admin",
+          name: name,
+          username: "Admin",
+          propic: null,
+        });
+
+        const newAdmin = await AdminDetailsModel.create({
+          userid: newUser._id,
+          email: email,
+          DOB: DOB,
+          mobilenumber: mobilenumber,
+        });
+
+        return res.status(201).json({
+          message: "Successfully Added!",
+          name: newUser.name,
+          email: newAdmin.email,
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({
+        status: "error",
+        message: "Error Occured!",
       });
     }
-  } catch (err) {
-    return res.status(500).json({
-      status: "error",
-      message: "Error Occured!",
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized!",
+      status: 401,
     });
   }
 };
@@ -118,159 +123,199 @@ const handleUpdateAdmin = async (req, res) => {
 };
 
 const getAllAdmins = async (req, res) => {
-  // console.log("hello");
-  const out = [];
+  const { usertype } = req.data;
+  if (usertype === "Super Admin") {
+    const out = [];
 
-  try {
-    const users = await UserModel.find({ usertype: "Admin" });
-    for (var i = 0; i < users.length; i++) {
-      const admin = await AdminDetailsModel.findOne({
-        userid: users[i]._id,
-      }).populate("userid");
-      out.push(admin);
+    try {
+      const users = await UserModel.find({ usertype: "Admin" });
+      for (var i = 0; i < users.length; i++) {
+        const admin = await AdminDetailsModel.findOne({
+          userid: users[i]._id,
+        }).populate("userid");
+        out.push(admin);
+      }
+
+      // console.log(out);
+      return res.status(200).json({
+        status: "success",
+        data: out,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error Occured!",
+      });
     }
-
-    // console.log(out);
-    return res.status(200).json({
-      status: "success",
-      data: out,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured!",
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized!",
+      status: 401,
     });
   }
 };
 
 const getAdminDetails = async (req, res) => {
-  // console.log("handle get admin calling, ", req.body);
   const { walletaddress } = req.body;
-  // console.log("passing data", walletaddress);
+  const { usertype } = req.data;
+  if (usertype === "Admin") {
+    try {
+      const user = await UserModel.findOne({ walletaddress: walletaddress });
+      // console.log("user", user);
 
-  try {
-    const user = await UserModel.findOne({ walletaddress: walletaddress });
-    // console.log("user", user);
-
-    const admin = await AdminDetailsModel.findOne({
-      userid: user._id,
-    }).populate("userid");
-    // console.log("admin", admin);
-    return res.status(200).json({
-      message: "Successfully Fetched!",
-      data: admin,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: "Error Occured!",
+      const admin = await AdminDetailsModel.findOne({
+        userid: user._id,
+      }).populate("userid");
+      // console.log("admin", admin);
+      return res.status(200).json({
+        message: "Successfully Fetched!",
+        data: admin,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: "Error Occured!",
+      });
+    }
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized!",
+      status: 401,
     });
   }
 };
 
 const deleteAdmin = async (req, res) => {
-  // console.log("hello delete");
-  try {
-    const { id } = req.params;
-    // console.log(id);
+  const { usertype } = req.data;
+  if (usertype === "Super Admin") {
+    try {
+      const { id } = req.params;
+      // console.log(id);
 
-    const admin = await AdminDetailsModel.deleteOne({ userid: id });
-    const user = await UserModel.deleteOne({ _id: id });
+      const admin = await AdminDetailsModel.deleteOne({ userid: id });
+      const user = await UserModel.deleteOne({ _id: id });
 
-    return res.status(200).json({
-      message: "Successfully Deleted!",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured!",
+      return res.status(200).json({
+        message: "Successfully Deleted!",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error Occured!",
+      });
+    }
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized!",
+      status: 401,
     });
   }
 };
 
 const getAdminRequests = async (req, res) => {
   const out = [];
+  const { usertype } = req.data;
+  if (usertype === "Super Admin") {
+    try {
+      const requests = await AdminUpdatingDetailsModel.find();
 
-  try {
-    const requests = await AdminUpdatingDetailsModel.find();
+      for (var i = 0; i < requests.length; i++) {
+        const user = await UserModel.findOne({ _id: requests[i].userid });
+        const admin = await AdminDetailsModel.findOne({
+          userid: requests[i].userid,
+        });
+        const obj = {
+          userid: requests[i].userid,
+          walletaddress: user.walletaddress,
+          name: user.name,
+          DOB: admin.DOB,
+          exist: {
+            email: admin.email,
+            mobilenumber: admin.mobilenumber,
+            propic: admin.propic,
+          },
+          new: {
+            email: requests[i].email,
+            mobilenumber: requests[i].mobilenumber,
+            propic: requests[i].propic,
+          },
+        };
+        out.push(obj);
+      }
 
-    for (var i = 0; i < requests.length; i++) {
-      const user = await UserModel.findOne({ _id: requests[i].userid });
-      const admin = await AdminDetailsModel.findOne({
-        userid: requests[i].userid,
+      return res.status(200).json({
+        status: "ok",
+        message: "Successfully Fetched!",
+        data: out,
       });
-      const obj = {
-        userid: requests[i].userid,
-        walletaddress: user.walletaddress,
-        name: user.name,
-        DOB: admin.DOB,
-        exist: {
-          email: admin.email,
-          mobilenumber: admin.mobilenumber,
-          propic: admin.propic,
-        },
-        new: {
-          email: requests[i].email,
-          mobilenumber: requests[i].mobilenumber,
-          propic: requests[i].propic,
-        },
-      };
-      out.push(obj);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error Occured!",
+      });
     }
-
-    return res.status(200).json({
-      status: "ok",
-      message: "Successfully Fetched!",
-      data: out,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured!",
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized!",
+      status: 401,
     });
   }
 };
 
 const approveRequest = async (req, res) => {
-  // console.log("hello delete");
-  try {
-    const { id } = req.params;
-    const request = await AdminUpdatingDetailsModel.findOneAndDelete({
-      userid: id,
-    });
-    const user = await UserModel.findOneAndUpdate(
-      { _id: id },
-      { propic: request.propic },
-      { new: true }
-    );
-    const admin = await AdminDetailsModel.findOneAndUpdate(
-      { userid: id },
-      { email: request.email, mobilenumber: request.mobilenumber },
-      { new: true }
-    );
+  const { usertype } = req.data;
+  if (usertype === "Super Admin") {
+    try {
+      const { id } = req.params;
+      const request = await AdminUpdatingDetailsModel.findOneAndDelete({
+        userid: id,
+      });
+      const user = await UserModel.findOneAndUpdate(
+        { _id: id },
+        { propic: request.propic },
+        { new: true }
+      );
+      const admin = await AdminDetailsModel.findOneAndUpdate(
+        { userid: id },
+        { email: request.email, mobilenumber: request.mobilenumber },
+        { new: true }
+      );
 
-    return res.status(200).json({
-      data: { user: user, admin: admin },
-      message: "Successfully Approved!",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured!",
+      return res.status(200).json({
+        data: { user: user, admin: admin },
+        message: "Successfully Approved!",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error Occured!",
+      });
+    }
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized!",
+      status: 401,
     });
   }
 };
 
 const declineRequest = async (req, res) => {
-  // console.log("hello delete");
-  try {
-    const { id } = req.params;
-    const request = await AdminUpdatingDetailsModel.findOneAndDelete({
-      userid: id,
-    });
+  const { usertype } = req.data;
+  if (usertype === "Super Admin") {
+    try {
+      const { id } = req.params;
+      const request = await AdminUpdatingDetailsModel.findOneAndDelete({
+        userid: id,
+      });
 
-    return res.status(200).json({
-      data: request,
-      message: "Successfully Rejected!",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured!",
+      return res.status(200).json({
+        data: request,
+        message: "Successfully Rejected!",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error Occured!",
+      });
+    }
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized!",
+      status: 401,
     });
   }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,53 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 const authController = require("../controllers/authController");
 
-router.post("/add-admin", adminController.handleAddAdmin);
+router.post(
+  "/add-admin",
+  authController.authenticate,
+  adminController.handleAddAdmin
+);
 
-router.post("/update-admin-details", authController.authenticate ,adminController.handleUpdateAdmin);
+router.post(
+  "/update-admin-details",
+  authController.authenticate,
+  adminController.handleUpdateAdmin
+);
 
-router.get("/get-admin-details", adminController.getAdminDetails);
-router.get("/get-admin-requests", adminController.getAdminRequests);
-router.post("/approve-request/:id", adminController.approveRequest);
-router.delete("/decline-request/:id", adminController.declineRequest);
+router.get(
+  "/get-admin-details",
+  authController.authenticate,
+  adminController.getAdminDetails
+);
+router.get(
+  "/get-admin-requests",
+  authController.authenticate,
+  adminController.getAdminRequests
+);
+router.post(
+  "/approve-request/:id",
+  authController.authenticate,
+  adminController.approveRequest
+);
+router.delete(
+  "/decline-request/:id",
+  authController.authenticate,
+  adminController.declineRequest
+);
 router
-  .post("/add-admin", adminController.handleAddAdmin)
-  .get("/get-all-admins", adminController.getAllAdmins)
-  .delete("/delete-admin/:id", adminController.deleteAdmin);
+  .post(
+    "/add-admin",
+    authController.authenticate,
+    adminController.handleAddAdmin
+  )
+  .get(
+    "/get-all-admins",
+    authController.authenticate,
+    adminController.getAllAdmins
+  )
+  .delete(
+    "/delete-admin/:id",
+    authController.authenticate,
+    adminController.deleteAdmin
+  );
 
 module.exports = router;
