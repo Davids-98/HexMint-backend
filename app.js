b04ecca05a05abe8e4629ca9c8f1cebce206fd18
Thilina Sandakelum <thilinas.19@cse.mrt.ac.lk>
2022-11-18 20:38:04 +0530

    added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const bodyParser = require("body-parser");
 const multer = require("multer");
 
@@ -19,9 +19,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json({ mssg: "Welcome to the app!" });
 });
-app.use("/auth", require("./routes/auth"));
-app.use("/user", require("./routes/user"));
-app.use("/customer", multer().array(), require("./routes/customer"));
-app.use("/admin", multer().array(), require("./routes/admin"));
+// app.use("/auth", require("./routes/auth"));
+// app.use("/user", require("./routes/user"));
+// app.use("/customer", multer().array(), require("./routes/customer"));
+// app.use("/admin", multer().array(), require("./routes/admin"));
 
 module.exports = app;
