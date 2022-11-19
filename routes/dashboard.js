999bf52afaf6489021758b41004b11252c179f79
Thilina Sandakelum <thilinas.19@cse.mrt.ac.lk>
2022-11-20 00:59:39 +0530

    added

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,19 +4,16 @@ const userController = require("../controllers/dashboardController");
 const authController = require("../controllers/authController");
 
 router.get(
-  "/get-nft-count",
+  "/get-nft-count/:activityType",
   authController.authenticate,
   userController.getNFTCount
 );
 
 router.get(
-  "/get-balance",
+  "/get-balance/:balanceType",
   authController.authenticate,
   userController.getBalance
 );
 
-router.get(
-  "/get-top-users",
-  userController.geTopUsers
-);
+router.get("/get-top-users/:userType", userController.geTopUsers);
 module.exports = router;
